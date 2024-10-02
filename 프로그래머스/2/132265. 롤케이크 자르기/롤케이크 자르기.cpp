e832adc2ad29336d8ae1e7b68ebf620abdafdd8b
redhean <48268986+redhean@users.noreply.github.com>
2024-10-02 18:04:10 +0900

    [level 2] Title: 롤케이크 자르기, Time: 212.41 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,44 +1,36 @@
 #include <string>
-#include <cstring>
 #include <vector>
-#include <iostream>
+#include <map>
 
 #define MAX 10001
 
 using namespace std;
-int arr1[MAX];
-int arr2[MAX];
-
-bool chk() {
-    int first = 0;
-    int second = 0;
-    
-    for(int i = 0; i < MAX; i++) {
-        if(arr1[i] != 0)
-            first++;
-        
-        if(arr2[i] != 0) {
-            second++;
-        }
-    }   
-    
-    return first == second;
-}
 
 int solution(vector<int> topping) {
     int answer = 0;
-    
-    for(int i = 0; i < topping.size(); i++) {
-        arr2[topping[i]]++;
+    map<int, int> a;
+    map<int, int> b;
+
+    for(int i = 0; i < topping.size(); i++)
+    {
+        b[topping[i]]++;
     }
-    
-    for(int i = 0; i < topping.size(); i++) {
-        arr1[topping[i]]++;
-        arr2[topping[i]]--;
-        
-        if(chk())
-            answer++;
+
+    for(int i = 0; i < topping.size(); i++)
+    {
+        b[topping[i]]--;
+        a[topping[i]]++;
+
+        if(b[topping[i]] == 0)
+        {
+            b.erase(topping[i]);
+        }
+
+        if(a.size() == b.size())
+        {
+            ++answer;
+        }
     }
-    
+
     return answer;
 }
