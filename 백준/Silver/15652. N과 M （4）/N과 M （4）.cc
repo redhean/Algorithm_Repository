f681e2c6f18a319e39f336adc0f55175816ef7c5
redhean <48268986+redhean@users.noreply.github.com>
2024-10-12 20:10:33 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+
+void dfs(int start) {
+    if (arr.size() == M) {
+        for (int a : arr) {
+            cout << a << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start; i <= N; i++) {
+        arr.push_back(i); 
+        dfs(i);            
+        arr.pop_back();  
+    }
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> N >> M;
+
+    dfs(1); 
+
+    return 0;
+}
